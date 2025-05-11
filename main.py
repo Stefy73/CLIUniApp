906641ceac9c70ddfdcb9b5060238e31fb05def0
aliciawze <alicia.w.zien@gmail.com>
2025-05-11 15:44:46 +1000

    testing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-# main.py
+# main.py 
 from student_controller import student_menu
 from admin_controller import admin_menu
 from database import Database 
-import re
 
 def university_menu():
     db = Database()
